44ddd516cd8169d7d581eb871f39e71a344bc315
Herbert Ilhan Tanujaya <mail@herbert.id>
2017-12-28 22:24:11 +0800

    Day 24 done

--- src/day24.rs
+++ src/day24.rs
@@ -19,15 +19,19 @@ mod tests {
     );
   }
 
-  #[ignore]
   #[test]
   fn no2_sample_test() {
     assert_eq!(
-      9,
+      19,
       run2(indoc!(
-        "5 9 2 8
-         9 4 7 3
-         3 8 6 5"
+        "0/2
+         2/2
+         2/3
+         3/4
+         3/5
+         0/1
+         10/1
+         9/10"
       ))
     );
   }
@@ -35,13 +39,12 @@ mod tests {
   #[ignore]
   #[test]
   fn no1_test() {
-    assert_eq!(21845, no1());
+    assert_eq!(1940, no1());
   }
 
-  #[ignore]
   #[test]
   fn no2_test() {
-    assert_eq!(191, no2());
+    assert_eq!(1928, no2());
   }
 }
 
@@ -73,8 +76,22 @@ fn run1(ports: &str) -> i32 {
   get_max_connection(p, 0)
 }
 
-fn run2(input: &str) -> i32 {
-  0
+fn run2(ports: &str) -> i32 {
+  let p = ports
+    .lines()
+    .map(|l| {
+      let mut sp = l.split("/");
+      (
+        sp.next().expect("Should have two ports").parse().expect(
+          "Pins should be integer",
+        ),
+        sp.next().expect("Should have two ports").parse().expect(
+          "Pins should be integer",
+        ),
+      )
+    })
+    .collect();
+  get_max_connection_with_len(p, 0).1
 }
 
 fn get_max_connection(ports: Vec<(i32, i32)>, start: i32) -> i32 {
@@ -85,9 +102,10 @@ fn get_max_connection(ports: Vec<(i32, i32)>, start: i32) -> i32 {
         return None;
       }
 
-      let other = p.0 + p.1 - start;
+      let pin_sum = p.0 + p.1;
+      let other = pin_sum - start;
       Some(
-        p.0 + p.1 +
+        pin_sum +
           get_max_connection(
             ports.iter().cloned().filter(|port| port != p).collect(),
             other,
@@ -96,4 +114,24 @@ fn get_max_connection(ports: Vec<(i32, i32)>, start: i32) -> i32 {
     })
     .max()
     .unwrap_or(0)
+}
+
+fn get_max_connection_with_len(ports: Vec<(i32, i32)>, start: i32) -> (i32, i32) {
+  ports
+    .iter()
+    .filter_map(|p| {
+      if p.0 != start && p.1 != start {
+        return None;
+      }
+
+      let pin_sum = p.0 + p.1;
+      let other = pin_sum - start;
+      let other_pins = ports.iter().cloned().filter(|port| port != p).collect();
+      let rest = get_max_connection_with_len(other_pins, other);
+
+      let res = Some((1 + rest.0, pin_sum + rest.1));
+      res
+    })
+    .max()
+    .unwrap_or((0, 0))
 }
